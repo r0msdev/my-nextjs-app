c3e8b7ed0ef3a863dc44e8d1d89fa2c3916ea280
sergios <sergios@gmail.com>
2026-01-29 09:15:01 +0100

    feat: integrate MUI and Emotion for styling
    - Added dependencies for @emotion and @mui/material in package.json.
    - Updated layout.tsx to use MUI's ThemeProvider and AppRouterCacheProvider.
    - Replaced Geist font with Roboto and adjusted font settings.
    - Created a new theme.ts file to define MUI theme with Roboto font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="en" className={roboto.variable}>
+      <body>
+      <AppRouterCacheProvider>
+        <ThemeProvider theme={theme}>          
+            {children}          
+        </ThemeProvider>
+      </AppRouterCacheProvider>
       </body>
     </html>
   );
